16b74f0ae0c994d58abe2bf383304f9c64c73b56
Sheeraz1982 <sheerazali@kss.net.pk>
2023-10-24 01:36:24 +0500

    New Commit

---
+++ Git1/app.js
@@ -0,0 +1,150 @@
+// Theory of GITHUB
+// github is a version control system
+// we can divdie our programming working within more than 2 people
+// we can assign jobs to three to four memebers through github
+// in software houses teams are working by using github after completion jobs we can push our coding on github then everyone can check working and edit as well
+// its just like a Developers cloud
+// whenever we go for an interview to any Software house, we can show our working from GITHUB
+// It is developer's portfolio 
+
+// Practical of GITHUB
+// Aik New Folder Banayenge apni directory main aur uska naam rkha GITHUB
+// GITHUB k folder main hum ne aik aur folder banaya jis ka naam rkha GIT1
+// us main index.html ki file banai
+// app.js ki aik file banai
+// file main aik website banayenge aur use GITHUB per upload karna hai
+// Repository per jayenge aur New repository per jayenge wahan uska naam denge koi bhi, Baaqi ka Data fill kar ke create Repository press kar denge
+
+// ab hum apne git1 k folder main jayenge aur use upload karenge GITHUB per
+// folder main right click karenge aur gitbashhere per jaenge
+
+// ab hum yahan commonds denge
+// Sub se pehli Commond git status hogi
+// ye enter karne k baad wo message de ga "not a git repository"
+// phir humen git repository banani pare gi
+// commond denge git init "Initialize karna"
+// is commond k baad ye Initialize kar de ga but empty hoga
+// ab hum phir git status chalenge new status k liay
+// phir git humen un-track folders ki details share kar dega
+// phir humen git ko apne folder ki detail deni hogi
+// phir aik commond chalaenge "git add ." jis se folder main saari chezun ko access kar dega
+// hum phir status lenge phir wo humen saari files show karaega
+// next step hum aik message denge github ko "git commit -m" 'Half work Done'
+// phir humen apni pehchan karani hai GITHUB ko (User name denge)
+// "git config --global user.name 'sheerazali1982"
+// phir hum apni email bhi denge ise
+// "git config --global user.email 'idealservicesprovider@gmail.com"
+// github main paste karne ke liay Shift+Enter press hoga 
+// git remote add origin https://github.com/sheerazali82/JS-Assignment.git
+// git push -u origin master (likh kar enter kar denge)
+// phir github main ja kar refresh karenge tu hamari files upload ho jayengi
+
+
+
+
+
+
+// console.log("Hello World");
+// console.log("4 + 4");
+// //Concatination
+// console.log("4" + "4")
+// // alert('Hello World');
+
+//variable: Aik Container hai
+// var names = 'Sheeraz';
+// console.log(names);
+// var number = ("Ali" + " " + "Jawwad");
+// console.log(number)
+// var number1 = 50 / 5
+// console.log(number1)
+// var firstNumber = 56
+// var secondNumber = 32
+// var thirdNumber = 44
+// console.log(firstNumber + secondNumber - thirdNumber)
+// console.log(thirdNumber * secondNumber - firstNumber)
+
+// 1.	Write a script to greet your website visitor using JS alert box.
+// alert('Error! Please enter valid password');
+
+// 3.	Write a script to display following message on your web page: (Hint : Use line break)
+// alert('Welcome to JS Land...\nHappy Coding');
+
+// var names = 'Sheeraz'
+// var age = 42
+
+// console.log("May Name is" + names + "and my age is" + age);
+
+// let userName = prompt('Enter your name');
+// let chemMarks = Number(prompt('Enter Chemistry marks'));
+// let mathMarks = Number(prompt('Enter Maths marks'));
+// let bioMarks = Number(prompt('Enter Biology marks'));
+// let engMarks = Number(prompt('Enter English marks'));
+
+// let totalMarks = 300;
+// let userTotal = chemMarks + mathMarks + bioMarks + engMarks
+// let percentage = userTotal / totalMarks *100;
+
+// // console.log(userName, chemMarks, mathMarks, bioMarks, engMarks);
+
+// console.log ('Student Name ' + userName + " got" + percentage + "%.");
+
+// if (percentage >= 80) {
+//     console.log('grade A+');
+// } else if (percentage <80 && percentage >= 70) {
+//     console.log('grade A');
+// } else if (percentage <70 && percentage >=60) {
+//     console.log('grade B');
+// } else if (percentage <60 && percentage >= 50) {
+//     console.log('grade C');
+// } else (percentage <50) {
+//     console.log("Fail");
+// }
+
+// Assignment Operators:
+
+// let num = 1;
+// num = num + 2;
+// num += 2
+// console.log(num);
+
+// Comparision Operators
+// let num1 = 10;
+// let num2 = 10;
+
+// let cond = num1 > num2;
+// let cond = num1 < num2;
+// let cond = num1 <= num2;
+// let cond = num1 >= num2;
+// let cond = num1 == num2;
+// let cond = num1 === num2;
+
+// console.log(cond);
+
+// Logical Operators
+
+//&&
+//||
+// let condition = 10 > 9 || 9 < 6;
+// let condition1 = 10 > 9 && 9 < 6;
+// console.log(condition1);
+
+// var myName = 'Syed Sheeraz Ali'
+// var intro = 'My Name is'
+// var myName1 = "Syed Fayyaz Ali"
+// var intro1 = 'My Father Name is'
+// var myName2 = "Syed Rashid Ali"
+// var intro2 = 'My Elder Borther Name is'
+// console.log(intro, myName, intro1, myName1, intro2, myName2);
+
+
+
+// If-Else condition practice:
+
+// If I play Game PACMAN on my mobile I want to apply some conditions on my score
+
+// const gameScore = 600;
+// if(gameScore < 500){
+// console.log(Loser);
+// }
+
+
